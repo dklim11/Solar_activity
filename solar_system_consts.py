bc08a501db941198767bb2fcef8786ab37d1c215
SchMichail <91534996+SchMichail@users.noreply.github.com>
2023-10-17 15:42:32 +0300

    .

--- solar_system_consts.py
+++ solar_system_consts.py
@@ -1,13 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Consts:
+# Consts:
 G = 6.6743  # 10**(-11) m^3/(kg*c^2)
 name = ['Sun', 'Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto']
 mass = [1.99*10**6, 0.32, 4.87, 5.98, 0.64, 1900, 569, 87, 103, 5]              # 10**(24) kg
-radius = [0, 57.9, 108.2, 149.6, 227.9, 778.6, 1433.7, 2870.4, 4491.1, 5868.9]  # 10**(9) m
-
+mass_ = [0.3329*10**6, 0.055274, 0.815005, 1, 0.012300, 0.10745, 317.83, 95.159, 14.500, 17.204, 0.0025]
+radius = [0, 57.9, 108.2, 149.6, 227.9, 778.6, 1433.7, 2870.4, 4491.1, 5868.9]  # 10**(9) m (distance to sun)
+radius_ = [0, 0.38710, 0.72333, 1.00001, 1.52363, 5.20441, 9.58378, 19.18722, 30.02090, 39.23107]   # a.e
+e = [0, 0.20564, 0.00676, 0.01672, 0.09344, 0.04890, 0.05689, 0.04634, 0.01129, 0.24448]    # eccentricity
 power = [0]*9
+
 for i in range(9):
     power[i] = (G * mass[i+1] * mass[0]) / (radius[i+1] ** 2)
 
@@ -23,12 +26,15 @@ for i in range(9):
     max_cntr += (mass[i+1] * radius[i+1]) / np.sum(mass)
 
 print(name)
-print(mass)
-print(radius)
+print("10**24 kg=  ", mass)
+print("mass of Earth=   ", mass_)
+print("distance to Sun=  ", radius)
+print("distance in a.e=  ", radius_)
+print("eccentricity", e)
 print(name[1:])
-print("power:", power)
+print("power:  ", power)
 print("min_cntr = ", min_cntr, "max_cntr = ", max_cntr)
-print(mass[5]*radius[5]/np.sum(mass))
+
 
 
 
